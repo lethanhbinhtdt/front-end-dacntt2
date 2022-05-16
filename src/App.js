4e1159a9be069d23e056728c165ebb0ca63c5f3b
Lê Thanh Bình <lethanhbinhtdt@gmail.com>
2022-05-16 20:45:55 +0700

    add route, check login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import React from 'react';
-import LoginPage from './components/login/LoginPage'
+import React, { useState, useEffect, Fragment } from 'react';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import axios from './middlewares/axios';
+
+import LoginPage from './components/login/LoginPage';
+import HomePage from './components/network/HomePage';
+import PersonalPage from './components/network/PersonalPage';
+
+import PublicRoute from './middlewares/PublicRoute';
+import PrivateRoute from './middlewares/PrivateRoute';
+import { getToken, setUserSession, removeUserSession } from './middlewares/common';
+
 import './App.css';
 
 function App() {
+  // const [authLoading, setAuthLoading] = useState(0);
+
+  // useEffect(() => {
+  //   const token = getToken();
+  //   if (!token) {
+  //     return;
+  //   }
+  // })
+
   return (
-    <div>
-      <LoginPage />
+    <div className="App">
+      <BrowserRouter>
+        <Fragment>
+          <div>
+            <div className="header">
+              <NavLink to="/" className={({ isActive }) => (isActive ? "active-style" : 'none')}>Home Page /</NavLink>
+              <NavLink to="/login" className={({ isActive }) => (isActive ? "active-style" : 'none')}>Login Page /</NavLink>
+              <NavLink to="/personal" className={({ isActive }) => (isActive ? "active-style" : 'none')}>Personal Page</NavLink>
+            </div>
+            <div className="content">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+
+                <Route element={<PublicRoute />}>
+                  <Route path='/login' element={<LoginPage />} />
+                </Route>
+
+                <Route element={<PrivateRoute />}>
+                  <Route path='/personal' element={<PersonalPage />} />
+                  <Route path='/personal2' element={<PersonalPage />} />
+                </Route>
+              </Routes>
+            </div>
+          </div>
+        </Fragment>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,16 +1,19 @@
-import React, { useState, useContext } from 'react';
-import AuthContext from '../context/AuthProvider';
-
+import React, { useState } from 'react';
 import axios from '../../middlewares/axios';
+import { useNavigate, useLocation } from 'react-router-dom'
+
 import { LOGIN_URL } from '../../middlewares/constant';
+import { setUserSession } from '../../middlewares/common'
 
 function LoginPage(props) {
-    const { setAuth } = useContext(AuthContext)
 
     const username = useFormInput('');
     const password = useFormInput('');
     const [errMsg, setErrMsg] = useState(null);
-    const [success, setSuccess] = useState(false);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const redirectPath = location.state?.path || '/';
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -24,40 +27,35 @@ function LoginPage(props) {
                 }
             );
             console.log(JSON.stringify(response?.data));
-            const accessToken = response?.data?.token;
-            setAuth({ username, password, accessToken });
-            setSuccess(true);
-            console.log(username, success, errMsg);
+            setUserSession(response?.data?.token, response?.data?.userInfo)
+            navigate(redirectPath, { replace: true });
+            
         } catch (err) {
             console.log(err);
             if (err.response.status === 400 || err.response.status === 401)
                 setErrMsg(err.response.data.description);
-            else 
+            else
                 setErrMsg("Something went wrong. Please try again later.");
         }
     }
 
     return (
-        <>
-            {success ? alert("Đăng nhập thành công") : (
-                <div className='login'>
-                    <div className='head'>
-                        <h1 className='company'>Đại học Tôn Đức Thắng</h1>
-                    </div>
-                    <form className="form" onSubmit={handleSubmit}>
-                        <div className='form-group'>
-                            <input type="text" name="username" {...username} autoComplete="off" required placeholder="Tài khoản" />
-                        </div>
-                        <div className='form-group'>
-                            <input type="password" name="password" {...password} required placeholder="••••••••••••••" />
-                        </div>
-                        <button type="submit" className='btn btn-login'>Đăng nhập</button>
-
-                        <p className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                    </form>
+        <div className='login'>
+            <div className='head'>
+                <h1>Đại học Tôn Đức Thắng</h1>
+            </div>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className='form-group'>
+                    <input type="text" name="username" {...username} autoComplete="off" required placeholder="Tài khoản" />
                 </div>
-            )}
-        </>
+                <div className='form-group'>
+                    <input type="password" name="password" {...password} required placeholder="••••••••••••••" />
+                </div>
+                <button type="submit" className='btn btn-login'>Đăng nhập</button>
+
+                <p className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+            </form>
+        </div>
     );
 
 }
