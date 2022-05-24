1da829f374d2b7d78d277ecc0759119f910d50fc
Lê Thanh Bình <lethanhbinhtdt@gmail.com>
2022-05-24 20:57:37 +0700

    .

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-function HomePage() {
+import PostCard from '../postCard/PostCard';
+import SideBar from '../layout/SideBar';
+
+function HomePage(props) {
+
     return (
         <div>
-            Welcome to the Home Page! - Don't need Login Demo
+            {/* Welcome {user.username}!<br /><br /> - Need Login Demo */}
+            {/* <input type='button' onClick={handleLogout} value='Logout' /> */}
+            <div className='row mt-3'>
+                <div className='col-md-3'>
+                    {/* <SideBar/> */}
+                </div>
+                <div className='col-md-6'>
+                    <div className='mb-3 mx-2'><PostCard/></div>
+                    <div className='mb-3 mx-2'><PostCard/></div>
+                    <div className='mb-3 mx-2'><PostCard/></div>
+                </div>
+
+                <div className='col-md-3'></div>
+            </div>
         </div>
     );
 }
--- src/components/personal/PersonalPage.js
+++ src/components/personal/PersonalPage.js
@@ -1,32 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { getUser, removeUserSession } from '../../middlewares/common';
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
 
-import PostCard from '../postCard/PostCard';
-
-function PersonalPage(props) {
-    const user = getUser();
-    const navigate = useNavigate()
-    // handle click event of logout button
-    const handleLogout = () => {
-        removeUserSession();
-        navigate('/login', { replace: true })
-    }
-
+function PersonalPage() {
     return (
         <div>
-            {/* Welcome {user.username}!<br /><br /> - Need Login Demo */}
-            {/* <input type='button' onClick={handleLogout} value='Logout' /> */}
-            <div className='row mt-3'>
-                <div className='col-md-3'></div>
-                <div className='col-md-6'>
-                    <div className='mb-3 mx-3'><PostCard/></div>
-                    <div className='mb-3 mx-3'><PostCard/></div>
-                    <div className='mb-3 mx-3'><PostCard/></div>
-                </div>
-
-                <div className='col-md-3'></div>
-            </div>
+            Welcome to the Personal Page!
         </div>
     );
 }
