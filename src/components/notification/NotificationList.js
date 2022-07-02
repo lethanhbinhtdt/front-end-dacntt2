b1a4db49b53936fe36bc66fbfab94daf2a4c2629
thinhnx <thinhgood9@gmail.com>
2022-07-02 12:00:48 +0700

    fix config noty

--- src/components/notification/NotificationList.js
+++ src/components/notification/NotificationList.js
@@ -1,46 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { BASE_URL } from '../../middlewares/constant';
 import { getCookieToken } from '../../middlewares/common'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faX } from '@fortawesome/free-solid-svg-icons'
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 function NotificationList(props) {
-    const { setNumberNotiNotChecked } = props
-    const now = new Date();
-    const token = getCookieToken()
-
-    const [notificationInfos, setNotificationInfo] = useState()
+    const { noifiInfos, setNoifiInfo, lenNotification,setNotificationInfo } = props
+    const [backgroundColor, setBackgrounColor] = useState(noifiInfos?.isChecked ? 'd-flex mb-2' : 'd-flex mb-2 backgroun-noti')
+    const [isChangeStatusNoti, setIsChangeStatusNoti] = useState(false)
     const [hasMorePost, setHasMorePost] = useState(true);
+    const now = new Date();
 
-    useEffect(() => {
-        fetch(`${BASE_URL}api/notification`, {
-            method: 'GET',
-            headers: {
-                'Content-type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }
-        })
-            .then(res => {
-                if (res.ok) {
-                    return res.json()
-                }
-            }).then(notification => {
-                setNotificationInfo(notification)
-                var numberNotCheck = 0
-                for (var i = 0; i <= notification?.length; i++) {
-                    if (!notification[i]?.isChecked) {
-                        numberNotCheck = numberNotCheck + 1
-                    }
-                }
-                setNumberNotiNotChecked(numberNotCheck)
+    const token = getCookieToken()
+    var listNoti = []
+    // useEffect(()=>{
+    //     if(noifiInfos){
+    //         setIsChangeStatusNoti(true)
+    //     }
 
-            })
-            .catch(err => {
-                console.error(err)
-            })
-    }, [])
+    // },[])
+
 
     const fetchMoreData = () => {
-        fetch(`${BASE_URL}api/notification/?skip=${notificationInfos.length}`, {
+        fetch(`${BASE_URL}api/notification/?skip=${lenNotification}`, {
             method: 'GET',
             headers: {
                 'Content-type': 'application/json',
@@ -57,19 +40,104 @@ function NotificationList(props) {
                     return;
                 }
 
-                setNotificationInfo([...notificationInfos, ...notification])
+                setNotificationInfo([...noifiInfos, ...notification])
                 var numberNotCheck = 0
                 for (var i = 0; i <= notification?.length; i++) {
                     if (!notification[i]?.isChecked) {
                         numberNotCheck = numberNotCheck + 1
                     }
                 }
-                setNumberNotiNotChecked(numberNotCheck)
+                // setNumberNotiNotChecked(numberNotCheck)
 
             })
             .catch(err => {
                 console.error(err)
             })
+    }
+    const handleChangeStatus = (e) =>{
+        const notiId = e.target.attributes.getNamedItem('notiid').value;
+        const token = getCookieToken()
+        const data = {
+            "notificationId": notiId
+        }
+        fetch(`${BASE_URL}api/notification/status`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body:JSON.stringify(data)
+
+        })
+            .then(res => {
+                if (res.ok) {
+                    return res.json()
+                }
+            }).then(notification => {
+                
+                for(var i =0; i<= noifiInfos?.length;i++){
+                    if(noifiInfos[i]?._id == notification?._id){
+                        noifiInfos[i] = notification
+                        setIsChangeStatusNoti(true)
+                    }
+                }
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }
+    // sử dụng useEffect và state isChangeStatusNoti để kiểm tra nếu có thay đổi trong list noti thì tiến hành cập nhật lại mà ko càn fetch lại dữ liệu 
+    // vì dữ liệu đã được thay thế ở hàm handleChangeStatus
+    useEffect(()=>{
+        if(isChangeStatusNoti){
+            for (var i = 0; i < noifiInfos?.length; i++) {
+                listNoti.push(
+                    <>
+                        <div key = {noifiInfos[i]?._id} className= {noifiInfos[i]?.isChecked ? 'd-flex mb-2' :'d-flex mb-2 backgroun-noti'}>
+                            <div className='notification-user-avatar'>
+                                <img alt='user avatar' src={noifiInfos[i]?.userIdGuest.picture}></img>
+                            </div>
+                            <div className='notification-content'>
+                                <div><b>{noifiInfos[i]?.userIdGuest?.fullname}</b> {noifiInfos[i]?.content}</div>
+                                <div className='fs-smaller text-secondary'>{now - noifiInfos[i]?.createdAt} giờ trước</div>
+                            </div>
+                            <div className='notification-button'>
+                                {noifiInfos[i]?.isChecked ?  <></>: <div><FontAwesomeIcon  notiid = {noifiInfos[i]?._id} icon={faCheck} color='green' onClick={handleChangeStatus}/></div>}
+                                <div><FontAwesomeIcon icon={faX} color='red' /></div>
+                            </div>
+        
+        
+                        </div>
+                        <hr></hr>
+                    </>
+                )
+            }
+            setIsChangeStatusNoti(false)
+        }
+      
+    }, [isChangeStatusNoti])
+   
+    for (var i = 0; i < noifiInfos?.length; i++) {
+        listNoti.push(
+            <>
+                <div key = {noifiInfos[i]?._id} className= {noifiInfos[i]?.isChecked ? 'd-flex mb-2' :'d-flex mb-2 backgroun-noti'}>
+                    <div className='notification-user-avatar'>
+                        <img alt='user avatar' src={noifiInfos[i]?.userIdGuest.picture}></img>
+                    </div>
+                    <div className='notification-content'>
+                        <div><b>{noifiInfos[i]?.userIdGuest?.fullname}</b> {noifiInfos[i]?.content}</div>
+                        <div className='fs-smaller text-secondary'>{now - noifiInfos[i]?.createdAt} giờ trước</div>
+                    </div>
+                    <div className='notification-button'>
+                        {noifiInfos[i]?.isChecked ?  <></>: <div><FontAwesomeIcon  notiid = {noifiInfos[i]?._id} icon={faCheck} color='green' onClick={handleChangeStatus}/></div>}
+                        <div><FontAwesomeIcon icon={faX} color='red' /></div>
+                    </div>
+
+
+                </div>
+                <hr></hr>
+            </>
+        )
     }
 
     return (
@@ -79,14 +147,14 @@ function NotificationList(props) {
         >
             {/*Put the scroll bar always on the bottom*/}
             <InfiniteScroll
-                dataLength={notificationInfos?.length || 0}
+                dataLength={lenNotification || 0}
                 next={fetchMoreData}
                 hasMore={hasMorePost}
                 loader={<p className='text-info'>Đang tải thông báo...</p>}
                 scrollableTarget='scrollableDiv'
             >
-
-                {notificationInfos && notificationInfos.map((item) => (
+                {listNoti}
+                {/* {notificationInfos && notificationInfos.map((item) => (
                     <div className='d-flex mb-2'>
                         <div className='notification-user-avatar'>
                             <img alt='user avatar' src={item?.userIdGuest.picture}></img>
@@ -96,10 +164,15 @@ function NotificationList(props) {
                             <div className='fs-smaller text-secondary'>{now - item?.createdAt} giờ trước</div>
                         </div>
                     </div>
-                ))}
+                ))} */}
             </InfiniteScroll>
         </div>
     );
+    // return (
+    //     <div>
+    //         {listNoti}
+    //     </div>
+    // );
 }
 
 export default NotificationList;
